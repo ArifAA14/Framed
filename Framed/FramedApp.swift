34409b537eb70e084252e255638e0ba7cbdee047
ArifAA14 <riffyrr45@gmail.com>
2024-11-21 17:09:50 +0300

    Initial Commit

---
+++ Framed/FramedApp.swift
@@ -0,0 +1,17 @@
+//
+//  FramedApp.swift
+//  Framed
+//
+//  Created by Ali on 21/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct FramedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FramedTests/FramedTests.swift
@@ -0,0 +1,17 @@
+//
+//  FramedTests.swift
+//  FramedTests
+//
+//  Created by Ali on 21/11/2024.
+//
+
+import Testing
+@testable import Framed
+
+struct FramedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
